faa2bd470c329379c136663c434ea84f6bd049cf
sarukhanmovi-star <sarukhanmovi@gmail.com>
2026-07-13 23:56:09 +0530

    Fix: Add Vercel preset to react-router config

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,10 @@
 import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
-	ssr: true,
-	future: {
-		unstable_viteEnvironmentApi: true,
-	},
+  presets: [vercelPreset()],
+  ssr: true,
+  future: {
+    unstable_viteEnvironmentApi: true,
+  },
 } satisfies Config;
